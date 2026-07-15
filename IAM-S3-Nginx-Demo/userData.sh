b612cc77df5500f43442f3b5081fdf3acc3fd5f8
Craig Woodcock <cwoodcock@spartaglobal.com>
2026-07-15 09:14:15 +0100

    editing commands and image extensions

--- IAM-S3-Nginx-Demo/userData.sh
+++ IAM-S3-Nginx-Demo/userData.sh
@@ -3,26 +3,26 @@
 set -e
 
 echo "Updating package lists..."
-apt update -y
+sudo apt update -y
 
 echo "Upgrading packages..."
-DEBIAN_FRONTEND=noninteractive apt upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 
 echo "Installing Nginx..."
-apt install nginx -y
+sudo DEBIAN_FRONTEND=noninteractive apt install nginx -y
 
 echo "Installing AWS CLI..."
-apt install awscli -y
+sudo DEBIAN_FRONTEND=noninteractive apt install awscli -y
 
 echo "Removing default website..."
-rm -rf /var/www/html/*
+sudo rm -rf /var/www/html/*
 
 echo "Downloading website from S3..."
 
-aws s3 cp s3://craig-iam-s3-nginx-bucket/website/ /var/www/html/ --recursive
+sudo aws s3 cp s3://craig-iam-s3-nginx-bucket/website/ /var/www/html/ --recursive
 
 systemctl enable nginx
 
 systemctl restart nginx
 
-echo "Deployment Complete"craig-iam-s3-nginx-bucket
+echo "Deployment Complete"
